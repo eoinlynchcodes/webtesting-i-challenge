7a673c289dbd5ba499def72d327b4849e538fe72
eoinlynchcodes <eoin95lynch@gmail.com>
2020-03-25 15:56:53 +0000

    4 passing tests.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,24 +6,36 @@ module.exports = {
 };
 
 const item = {
-  name: '',
+  name: 'Eoin',
   enhancement: 10,
   durability: 50,
 }
 
 function succeed(item) {
-
-  return { ...item };
+  if(item.enhancement >= 20){
+    return {...item};
+  } else {
+  return {...item, enhancement: item.enhancement + 1};
+  }
 }
 
-function fail(item) {
-  return { ...item };
+function fail(item){
+  if(item.enhancement < 15){
+    return {...item, durability: item.durability - 5};
+  } else if(item.enhancement >= 16){
+    return {...item, durability: item.durability -10};
+  } else if(item.enhancement > 15){
+    return {...item, durability: item.durability -1 }
+  }
 }
 
-function repair(item) {
-  return { ...item };
+function repair(item) { 
+  return { ...item, durability: item.durability = 100 };
 }
 
 function get(item) {
   return { ...item };
 }
+
+
+get(item);
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,3 +1,33 @@
 const enhancer = require('./enhancer.js');
 // test away!
 
+const item = { name: 'Eoin', enhancement: 10, durability: 70};
+
+// const idealItem = {
+//     name: 'Eoin',
+//     enhancement: 10,
+//     durability: 100
+// };
+
+describe('repair', () => {
+    it('Repair sets item to 100.', () => {
+        expect(enhancer.repair(item)).toEqual({...item, durability: item.durability = 100 })}
+)});
+
+describe('success', () => {
+    it('It returns a new object with enhancement increased', () => {
+        expect(enhancer.succeed(item)).not.toBe({...item, enhancement: item.enhancement = false });
+    })
+});
+
+describe('fail', () => {
+    it('decreases the item.durability', () => {
+        expect(enhancer.fail(item)).not.toBe({...item, durability: item.durability > 50});
+    })
+});
+
+describe('get', () => {
+    it('gets all the items back', () => {
+        expect(enhancer.get(item)).toEqual(item);
+    })
+});
